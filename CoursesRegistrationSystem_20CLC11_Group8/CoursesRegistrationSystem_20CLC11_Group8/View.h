bff5b056a8d1140f096472daf2089f5f42bb261b
tqthinh16 <thaiduong161@gmail.com>
2021-05-15 21:59:19 +0700

    Update View.h

--- CoursesRegistrationSystem_20CLC11_Group8/CoursesRegistrationSystem_20CLC11_Group8/View.h
+++ CoursesRegistrationSystem_20CLC11_Group8/CoursesRegistrationSystem_20CLC11_Group8/View.h
@@ -1,5 +1,11 @@
 #ifndef View
 #define View
 #include "Data.h"
+
+void PrintHello(Student* pHead);
+void viewAllStuIn1Class(Class* pHead);
+void viewAllStuIn1Course(Courses* pHead);
+void ViewScore(Courses*& cou, wstring ID, wstring NoYear, int NoSem);
+void ViewSchoolyear(Schoolyear*& head);
 //=======
 #endif
