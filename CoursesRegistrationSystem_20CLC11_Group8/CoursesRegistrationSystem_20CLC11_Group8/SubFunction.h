d956ebe6d8c8159425e011bc36cc9150699051fb
tqthinh16 <thaiduong161@gmail.com>
2021-05-15 21:59:31 +0700

    Update SubFunction.h

--- CoursesRegistrationSystem_20CLC11_Group8/CoursesRegistrationSystem_20CLC11_Group8/SubFunction.h
+++ CoursesRegistrationSystem_20CLC11_Group8/CoursesRegistrationSystem_20CLC11_Group8/SubFunction.h
@@ -6,10 +6,15 @@
 
 #include <string>
 
+bool operator>= (const Date& a, const Date& b);
 
+bool operator<= (const Date& a, const Date& b);
+
+bool operator== (const Date& a, const Date& b);
+
+bool operator!= (const Date& a, const Date& b);
 
 Date OutputBirthday(std::wstring k);
-
 
 Date RealTime();
 
@@ -23,13 +28,11 @@ std::string NumToString(int n);
 
 int StringtoNum(std::string k);
 
-
-
 int WStringtoNum(std::wstring k);
 
 wchar_t* StringtoLongChar(std::string k);
 
-std::string WStringToString(std::wstring k);
+std::string WstringToString(std::wstring k);
 
 std::wstring StringToWString(std::string k);
 
@@ -38,5 +41,13 @@ std::wstring getpass();
 int CountCourses(Student* stu);
 
 void FixConsoleWindow();
+
+void CheckNumber(Student* pHead);
+
+bool checkRegis(Semester* sem, int NoSem);
+
+int CheckSemesterNo();
+
+float WStringtoFloat(std::wstring strbuf);
 
 #endif // !_Func
