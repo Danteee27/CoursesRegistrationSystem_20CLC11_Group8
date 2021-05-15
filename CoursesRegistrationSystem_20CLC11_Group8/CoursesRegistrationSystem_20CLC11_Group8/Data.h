c52475b50835b305d8fc8863abe64ec2c720933c
tqthinh16 <thaiduong161@gmail.com>
2021-05-15 21:59:57 +0700

    Update Data.h

--- CoursesRegistrationSystem_20CLC11_Group8/CoursesRegistrationSystem_20CLC11_Group8/Data.h
+++ CoursesRegistrationSystem_20CLC11_Group8/CoursesRegistrationSystem_20CLC11_Group8/Data.h
@@ -12,12 +12,13 @@
 #include <fcntl.h>
 #include <io.h>
 #include <filesystem>
-
+using namespace std;
 
 
 struct Date {
 	int day, month, year;
 };
+
 
 struct Score {
 	std::string courseCode;
@@ -27,6 +28,7 @@ struct Score {
 	float gpa = 0;
 	Score* next=nullptr, * prev=nullptr;
 };
+
 
 struct Student {
 	int Num;
@@ -67,23 +69,39 @@ struct Courses {
 struct Semester {
 	int No;
 	Date startDate, endDate;
-	Courses* Course;
+	Courses* Course = nullptr;
 	Semester* next=nullptr, * prev=nullptr;
 };
 
 struct Schoolyear {
 	std::wstring year;
-	Student* year_Student;
-	Semester* sem;
-	Schoolyear* next=nullptr, * prev=nullptr;
+	Semester* sem = nullptr;;
+	Student* year_Student = nullptr;
+	Class* all_Class = nullptr;
+	Schoolyear* next = nullptr, * prev = nullptr;
 };
 
+static Date Sem1_start = { 1, 9, 2020 };
+static Date Sem1_end = { 1,12, 2020 };
+static Date Sem2_start = { 1, 1, 2021 };
+static Date Sem2_end = { 1,4, 2021 };
+static Date Sem3_start = { 1, 5, 2021 };
+static Date Sem3_end = { 1,9, 2021 };
+
+void LoadSchoolYear(Schoolyear*& head);
 
 Student* ReadStudent(std::string k);
 
 void SaveStudent(Student* pHead, std::string k);
 
-
 Student* FindStudent(Student* head, std::wstring ID);
+
+void LoadRegisDate(Semester*& head, wstring NoYear);
+
+void RegisDate(Semester*& head, wstring NoYear);
+
+Courses* ouputCoursesbyID(Courses* pHead, std::string cID);
+
+void LoadClass(Schoolyear*& head);
 
 #endif // !_Func
